8d56b230eb35ab4cdea0c39907cf63524d26fe07
Rahul Harsh <rahulharsh017@gmail.com>
2024-10-05 19:15:12 +0530

    Ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import HomePage from './pages/Homepage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import ExplorePage from './pages/ExplorePage'
+import LikesPage from './pages/LikesPage'
+import Sidebar from './components/Sidebar'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex text-white'>
+       <Sidebar /> 
+      <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-100 flex-1'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/explore' element={<ExplorePage />} />
+          <Route path='/likes' element={<LikesPage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
