6eb14b56d5fcc47c7f91fe9a9981474125c54470
Rahul Harsh <rahulharsh017@gmail.com>
2024-11-19 19:20:37 +0530

    Refactor user controller and routes, add like profile feature

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,14 @@
 import express, { Router } from 'express';
 import { getUserProfileAndRepos } from '../controllers/user.controller.js';
+import {ensureAuthenticated} from '../middleware/ensureAuthenticated.js'
+import { likeProfile,getLikes } from '../controllers/user.controller.js';
 
 const router = Router();
 
 router.get('/profile/:username',getUserProfileAndRepos);
 
+router.get('likes',ensureAuthenticated,getLikes);
+
+router.post("/like/:username",ensureAuthenticated,likeProfile);
 
 export default router;
--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -5,6 +5,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
 import { formatMemberSince } from "../utils/function.js";
+import LikeProfile from "./LikeProfile.jsx";
 
 const ProfileInfo = ({userProfile}) => {
 	// const userProfile = {
@@ -33,6 +34,7 @@ const ProfileInfo = ({userProfile}) => {
 					</a>
 					{/* View on Github */}
 					<div className='flex gap-2 items-center flex-col'>
+						<LikeProfile userProfile={userProfile} />
 						<a
 							href={userProfile?.html_url}
 							target='_blank'
